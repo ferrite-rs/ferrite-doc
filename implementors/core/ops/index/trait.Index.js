2208fe34c17dd71eee1d629c12b3c64603812261
soareschen <soareschen@users.noreply.github.com>
2021-01-05 12:05:01 +0000

    deploy: ferrite-rs/ferrite@da7446fb4d17e80ae436f5e43dbc3c4799286a1c

--- implementors/core/ops/index/trait.Index.js
+++ implementors/core/ops/index/trait.Index.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["regex"] = [{"text":"impl&lt;'t&gt; Index&lt;usize&gt; for Captures&lt;'t&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'t, 'i&gt; Index&lt;&amp;'i str&gt; for Captures&lt;'t&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'t&gt; Index&lt;usize&gt; for Captures&lt;'t&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'t, 'i&gt; Index&lt;&amp;'i str&gt; for Captures&lt;'t&gt;","synthetic":false,"types":[]}];
-implementors["serde_json"] = [{"text":"impl&lt;'a, Q:&nbsp;?Sized&gt; Index&lt;&amp;'a Q&gt; for Map&lt;String, Value&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;String: Borrow&lt;Q&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;Q: Ord + Eq + Hash,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl&lt;I&gt; Index&lt;I&gt; for Value <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;I: Index,&nbsp;</span>","synthetic":false,"types":[]}];
 implementors["slab"] = [{"text":"impl&lt;T&gt; Index&lt;usize&gt; for Slab&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl&lt;T, P&gt; Index&lt;usize&gt; for Punctuated&lt;T, P&gt;","synthetic":false,"types":[]}];
 implementors["vec_arena"] = [{"text":"impl&lt;T&gt; Index&lt;usize&gt; for Arena&lt;T&gt;","synthetic":false,"types":[]}];
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -18,7 +18,6 @@ implementors["rand_core"] = [{"text":"impl Error for Error","synthetic":false,"t
 implementors["regex"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 implementors["regex_syntax"] = [{"text":"impl Error for Error","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]},{"text":"impl Error for CaseFoldError","synthetic":false,"types":[]},{"text":"impl Error for UnicodeWordError","synthetic":false,"types":[]}];
 implementors["serde"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde_json"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 implementors["tempfile"] = [{"text":"impl Error for PathPersistError","synthetic":false,"types":[]},{"text":"impl Error for PersistError","synthetic":false,"types":[]}];
 implementors["termcolor"] = [{"text":"impl Error for ParseColorError","synthetic":false,"types":[]}];
