ae8fd0dd80fdf5e545edf054254bd0b1d2aed53b
soareschen <soareschen@users.noreply.github.com>
2022-01-22 15:14:33 +0000

    deploy: ferrite-rs/ferrite@d3a8d3821b387a362eb8b2deaa916628b86ac858

--- ferrite_session/internal/functional/type_app/sidebar-items.js
+++ ferrite_session/internal/functional/type_app/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_applied",""],["wrap_type_app",""]],"struct":[["App",""]],"trait":[["HasTypeApp",""],["TyCon",""],["TypeApp",""]]});
+initSidebarItems({"struct":[["App",""]],"trait":[["HasTypeApp",""],["TyCon",""],["TypeApp",""]]});
